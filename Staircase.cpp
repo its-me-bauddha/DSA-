5e7622a9f4a055c76d75762521168cc4ccb63ed1
Harsh  Bauddha <105487622+its-me-bauddha@users.noreply.github.com>
2023-02-02 15:40:22 +0530

    Create Staircase.cpp

---
+++ Staircase.cpp
@@ -0,0 +1,15 @@
+void staircase(int n) {
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            cout <<" ";
+        }
+        for(int k = n-i ; k <=n;k++)
+        {
+            cout<<"#" ;
+        }
+        cout << endl;
+        
+    }
+}
